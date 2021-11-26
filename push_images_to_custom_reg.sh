5c8964f3a86f92872b7392f1483e79ba0f6b77f1
kangchengkun <460876880@qq.com>
2021-11-26 13:55:25 +0800

    Update docker files

--- push_images_to_custom_reg.sh
+++ push_images_to_custom_reg.sh
@@ -6,16 +6,30 @@ plain_dockerfile_path=./plain/go-1.16.x
 node_dockerfile_path=./node/go-1.16.x/node-14.18.0
 
 # docker login
+echo
+echo 'Login to custom docker registry...'
 docker login your-custom-reg-addr.your-org.com -u reg-user-name -p reg-user-pwd
 
 # build the docker image
+echo
+echo 'Build revel plain image...'
 docker build ${plain_dockerfile_path} -t your-custom-reg-addr.your-org.com/revel:${plain_image_version} -t your-custom-reg-addr.your-org.com/revel:latest
+echo
+echo 'Build revel node image...'
 docker build ${node_dockerfile_path} -t your-custom-reg-addr.your-org.com/revel-node:${node_image_version} -t your-custom-reg-addr.your-org.com/revel-node:latest
 
 # push images to docker hub
+echo
+echo 'Push revel plain latest to docker registry...'
 docker push your-custom-reg-addr.your-org.com/revel:latest
+echo
+echo 'Push revel node latest to docker registry...'
 docker push your-custom-reg-addr.your-org.com/revel-node:latest
+echo
+echo 'Push revel plain to docker registry...'
 docker push your-custom-reg-addr.your-org.com/revel:${plain_image_version}
+echo
+echo 'Push revel node latest to docker registry...'
 docker push your-custom-reg-addr.your-org.com/revel-node:${node_image_version}
 
 # remove from local registry
--- push_images_to_docker_hub.sh
+++ push_images_to_docker_hub.sh
@@ -6,13 +6,25 @@ plain_dockerfile_path=./plain/go-1.16.x
 node_dockerfile_path=./node/go-1.16.x/node-14.18.0
 
 # build the docker image
+echo
+echo 'Build revel plain image...'
 docker build ${plain_dockerfile_path} -t kangchengkun/revel:${plain_image_version} -t kangchengkun/revel:latest
+echo
+echo 'Build revel node image...'
 docker build ${node_dockerfile_path} -t kangchengkun/revel-node:${node_image_version} -t kangchengkun/revel-node:latest
 
 # push images to docker hub
+echo
+echo 'Push revel plain latest to docker registry...'
 docker push kangchengkun/revel:latest
+echo
+echo 'Push revel node latest to docker registry...'
 docker push kangchengkun/revel-node:latest
+echo
+echo 'Push revel plain to docker registry...'
 docker push kangchengkun/revel:${plain_image_version}
+echo
+echo 'Push revel node latest to docker registry...'
 docker push kangchengkun/revel-node:${node_image_version}
 
 # remove from local registry
